3e18315837777dacd8a79c2a03d38fc1ace864fb
Eoin Hearne <eoin.hearne@errigal.com>
2021-04-15 16:39:07 +0100

    added API Documentation

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -1,16 +1,23 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies/index.js';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
 
 dotenv.config();
 
 const app = express();
+
+const swaggerDocument = yaml.load(fs.readFileSync('./../movie-api-yaml/movie-api.yaml', 'utf8'));
 
 const port = process.env.PORT;
 
 app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
