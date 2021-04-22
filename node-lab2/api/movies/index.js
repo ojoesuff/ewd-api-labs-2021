d69ae57700e4a01b9d059f31da97f8a23d844485
Eoin Hearne <eoin.hearne@errigal.com>
2021-04-22 15:51:27 +0100

    added movie endpoint

--- node-lab2/api/movies/index.js
+++ node-lab2/api/movies/index.js
@@ -1,48 +1,32 @@
 import express from 'express';
-import {NotFound} from './../../responses';
-import uniqid from 'uniqid';
+import movieModel from './movieModel';
+import asyncHandler from 'express-async-handler';
+import { NotFound } from './../../responses';
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.status(200).json(movies);
+
+router.get('/', async (req, res) => {
+
+    let { page = 1, limit = 4 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const movies = await moviesPromise;
+
+    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
 });
 
-// Get a movie
 // Get movie details
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (movieDetails.id == id) {
-        res.status(200).json(movieDetails);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
-});
-
-// Get movie reviews
-router.get('/:id/reviews', (req, res) => {
-    const id = parseInt(req.params.id);
-    // find reviews in list
-    if (movieReviews.id == id) {
-        res.status(200).json(movieReviews);
-    } else {
-        res.status(404).json(NotFound);
-    }
-});
-
-//Post a movie review
-router.post('/:id/reviews', (req, res) => {
-    const id = parseInt(req.params.id);
-    // find reviews in list
-    if (movieReviews.id == id) {
-        req.body.created_at = new Date();
-        req.body.updated_at = new Date();
-        req.body.id = uniqid();
-
-        movieReviews.results.push(req.body);
-        res.status(201).json(req.body);
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const movie = await movieModel.findById(id).exec();
+    if (movie) {
+        res.status(200).json(movie);
     } else {
         res.status(404).json(NotFound);
     }
